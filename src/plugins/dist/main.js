27d9623d17173036999a8e759b05729b5817a54e
champkeh <champion.ge@gmail.com>
2021-03-27 21:13:00 +0800

    rollup: 更新rollup插件相关代码

--- src/plugins/dist/main.js
+++
@@ -1,5 +0,0 @@
-import { c as color, s as size } from './config-65f5a896.js';
-
-CSS.paintWorklet.addModule(new URL('worklet.js', import.meta.url).href);
-
-document.body.innerHTML += `<h1 style="background-image: paint(vertical-lines);">color: ${color}, size: ${size}</h1>`;
